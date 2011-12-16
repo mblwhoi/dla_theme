e40e921f90df225ce970700c2ae89d76640b4089
adorsk-whoi <adorsk\@whoi.edu>
2011-12-16 05:28:13 -0800

    took out animation that was causing ie errors

--- dla_theme/js/dla_theme_script.js
+++ dla_theme/js/dla_theme_script.js
@@ -1,8 +1,7 @@
 Drupal.behaviors.dla_themeSuperfish = function (context) {
     $("#dla-nav-menu").superfish({
             hoverClass:  'sfHover',
-            delay:       0,
-            animation:   {opacity:'none',height:'none'},
+            delay:       200,
             speed:       'fast',
             autoArrows:  false,
             dropShadows: false,
